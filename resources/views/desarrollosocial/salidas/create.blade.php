47d5c085c249ec8bf276e4688c589a6c527b52a6
Angel Reyna <sysmard@gmail.com>
2018-01-28 22:30:08 -0600

    Se modifico la parte final en la vista de salidas de apoyo implementando selects dinamicos

--- resources/views/desarrollosocial/salidas/create.blade.php
+++ resources/views/desarrollosocial/salidas/create.blade.php
@@ -19,7 +19,7 @@
                       {{-- {!! Form::label('NUMERO ESTADO:', 'NUMERO ESTADO:') !!}
                       {!! Form::text('id_estado', null, ['class' => 'form-control' , 'required' => 'required','placeholder'=>'NUMERO ESTADO']) !!} --}}
                       {!! Form::label("NUMERO ESTADO","NUMERO ESTADO") !!}
-                      <select class="form-control" id="id_estado">
+                      <select class="form-control" id="id_estado" name="id_estado" >
                         @foreach($estados as $estado)
                         <option>{{$estado->id_estado}}</option>
                         @endforeach
@@ -29,7 +29,7 @@
                   <div class="form-group">
                       {!! Form::label('NUMERO MUNICIPIO:', 'NUMERO MUNICIPIO:') !!}
                       {{-- {!! Form::text('id_municipio', null, ['class' => 'form-control' , 'required' => 'required','placeholder'=>'NUMERO MUNICIPIO']) !!}--}}
-                      <select class="form-control" id="id_municipio">
+                      <select class="form-control" id="id_municipio" name="id_municipio">
                         @foreach($municipios as $municipio)
                         <option>{{$municipio->id_municipio}}</option>
                         @endforeach
@@ -40,7 +40,7 @@
                       {!! Form::label('NUMERO LOCALIDAD:', 'NUMERO LOCALIDAD:') !!}
                       {{-- {!! Form::text('id_localidad', null, ['class' => 'form-control' , 'required' => 'required','placeholder'=>'NUMERO LOCALIDAD']) !!}
                        --}}
-                       <select class="form-control" id="id_localidad">
+                       <select class="form-control" id="id_localidad" name="id_localidad">
                         @foreach($localidad as $localidad)
                         <option>{{$localidad->id_localidad}}</option>
                         @endforeach
@@ -50,7 +50,7 @@
                   <div class="form-group">
                       {!! Form::label('NUMERO SECCION:', 'NUMERO SECCION:') !!}
                       {{-- {!! Form::text('id_seccion', null, ['class' => 'form-control' , 'required' => 'required','placeholder'=>'NUMERO SECCION']) !!} --}}
-                      <select class="form-control" id="id_seccion">
+                      <select class="form-control" id="id_seccion" name="id_seccion">
                         @foreach($seccion as $seccion)
                         <option>{{$seccion->id_seccion}}</option>
                         @endforeach
@@ -60,7 +60,7 @@
                   <div class="form-group">
                       {!! Form::label('TIPO DE APOYO:', 'TIPO DE APOYO:') !!}
                       {{-- {!! Form::text('id_apoyo', null, ['class' => 'form-control' , 'required' => 'required','placeholder'=>'TIPO DE APOYO']) !!} --}}
-                        <select class="form-control" id="id_apoyo">
+                        <select class="form-control" id="id_apoyo" name="id_apoyo">
                         @foreach($apoyo as $apoyo)
                         <option>{{$apoyo->id_apoyo}}</option>
                         @endforeach
